03dae2739d7c1d41e89169b8f3245a7c7fad3ba1
FrankenDeba <debashishrambhola@gmail.com>
2025-11-05 10:10:49 +0530

    initial placeholder file

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter three numbers: \n";
+    int a, b, c;
+
+    cin >> a >> b >> c;
+    cout << "Sum of three numbers is: " << a + b + c << endl;
+
+    return 0;
+}
